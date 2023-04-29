05cdccb473b4f6c103d0b57dc39e8234d37c3d5c
Dylan Alvandi <geradylanalvandi@gmail.com>
2023-04-29 19:33:17 +0200

    Frontend 2

--- frontend/ActivityCard.js
+++ frontend/ActivityCard.js
@@ -1,8 +1,32 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image, Modal } from 'react-native';
 import { Card, Button, Icon } from 'react-native-elements';
+import { Audio } from 'expo-av';
 
 const ActivityCard = ({ activity }) => {
+
+    const [currentlyPlaying, setCurrentlyPlaying] = useState(null);
+
+    async function PlayAudio() {
+
+        if (currentlyPlaying == null) {
+            const sound = new Audio.Sound();
+            setCurrentlyPlaying(sound);
+            await sound.loadAsync({
+                uri: "https://samplelib.com/lib/preview/mp3/sample-3s.mp3"
+            });
+            await sound.playAsync();
+            await sound.setOnPlaybackStatusUpdate((status) => {
+                if (!status.isPlaying) {
+                    setCurrentlyPlaying(null);
+                }
+            });
+        } else {
+            console.log("Audio currently playing, will not add another audio..");
+        }
+    }
+
+
     return (
         <>
             <Card>
@@ -13,9 +37,14 @@ const ActivityCard = ({ activity }) => {
                     {activity.UUID}
                 </Text>
                 {
-                    activity.file &&
-                    <Card.Image source={{uri:activity.file}}></Card.Image>
+                    activity.image &&
+                    <Card.Image source={{ uri: activity.image }}></Card.Image>
                 }
+                {
+                    activity.audio &&
+                    <Button onPress={PlayAudio}></Button>
+                }
+                
             </Card>
         </>
     );
--- frontend/ChronologyCard.js
+++ frontend/ChronologyCard.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Modal, Styles, StyleSheet, TextInput, Pressable, Alert } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { Button } from '@react-native-material/core';
 import ActivityCard from "./ActivityCard";
@@ -14,6 +14,13 @@ const ChronologyCard = ({ chronology }) => {
     const [image, setImage] = useState(null);
     const [imageName, setImageName] = useState(null);
 
+    const [audio, setAudio] = useState(null);
+    const [audioName, setAudioName] = useState(null);
+
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const [newActivityName, setNewActivityName] = useState('');
+
     const pickImage = async () => {
         let result = await DocumentPicker.getDocumentAsync({
             type: 'image/*',
@@ -24,13 +31,33 @@ const ChronologyCard = ({ chronology }) => {
                 order_number: 2023,
                 name: result.name,
                 UUID: uuid.v4(),
-                file: result.uri,
+                image: result.uri,
             };
             const newActivities = [...chronology.activities, newActivity];
             chronology.activities = newActivities;
             console.log(newActivities);
             setImage(result.uri);
             setImageName(result.name);
+        }
+    };
+
+    const pickAudio = async () => {
+        let result = await DocumentPicker.getDocumentAsync({
+            type: 'audio/*',
+            copyToCacheDirectory: false,
+        });
+        if (!result.cancelled) {
+            const newActivity = {
+                order_number: 2023,
+                name: result.name,
+                UUID: uuid.v4(),
+                audio: result.uri,
+            };
+            const newActivities = [...chronology.activities, newActivity];
+            chronology.activities = newActivities;
+            console.log(newActivities);
+            setAudio(result.uri);
+            setAudioName(result.name);
         }
     };
 
@@ -41,7 +68,7 @@ const ChronologyCard = ({ chronology }) => {
     return (
         <>
             <Card>
-                <Card.Image source={require('./assets/favicon.png')} />
+                {/* <Card.Image source={require('./assets/favicon.png')} /> */}
                 <Text style={{ fontSize: 22 }}>{chronology.name}</Text>
                 <Text style={{ fontSize: 10 }}>{chronology.time}</Text>
                 <Text style={{ marginBottom: 10 }}>
@@ -56,9 +83,9 @@ const ChronologyCard = ({ chronology }) => {
                         }
                         <Card wrapperStyle={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
                             <Button buttonStyle={{ borderRadius: 0, marginLeft: 5, marginRight: 5, marginBottom: 0 }}
-                                title={'Text'} onPress={() => { }} />
+                                title={'Text'} onPress={() => { setModalVisible(true) }} />
                             <Button buttonStyle={{ borderRadius: 0, marginLeft: 5, marginRight: 5, marginBottom: 0 }}
-                                title={'Voice'} onPress={() => { }} />
+                                title={'Voice'} onPress={pickAudio} />
                             <Button buttonStyle={{ borderRadius: 0, marginLeft: 5, marginRight: 5, marginBottom: 0 }}
                                 title={'Image'} onPress={pickImage} />
                         </Card>
@@ -77,9 +104,92 @@ const ChronologyCard = ({ chronology }) => {
                     onPress={toggleExpanded}
                     style={{ margin: 10, marginTop: 5 }}
                 />
+
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        setModalVisible(!modalVisible);
+                    }}>
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <Text style={styles.modalText}>New Activity</Text>
+                            <Text>Activity Name</Text>
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={(i) => { console.log("name input:" + i); setNewActivityName(i); }}
+                                placeholder={'e.g. "Get some pizza" '}
+                            />
+                            <Pressable
+                                style={[styles.button, styles.buttonClose]}
+                                onPress={() => {
+                                    const activityToAdd =
+                                        { "name": newActivityName, "UUID": uuid.v4() }
+                                    console.log(activityToAdd)
+
+                                    /*
+                                    Currently only adds to Report 0
+                                    */
+                                    chronology.activities.push(activityToAdd)
+
+                                    Alert.alert("Activity added"); setModalVisible(!modalVisible);
+                                }}>
+                                <Text style={styles.textStyle}>Add</Text>
+                            </Pressable>
+                            <Pressable
+                                style={[styles.button, styles.buttonClose]}
+                                onPress={() => { setModalVisible(!modalVisible); }}>
+                                <Text style={styles.textStyle}>Cancel</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                </Modal>
+
             </Card>
         </>
     );
 }
+
+const styles = StyleSheet.create({
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    modalView: {
+        margin: 5,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 10,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonOpen: {
+        backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+        backgroundColor: '#2196F3',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+});
 
 export default ChronologyCard;
